32df46d5c253ac64038346fa6fd5ee75ce56ad6b
Rimvydas Petkus <rimvydas@zenitech.co.uk>
2020-02-22 12:03:35 +0200

    draft version of phone bill calculation implemented

--- src/main/java/com/javacourses/learning/PhoneBillCalculator.java
+++ src/main/java/com/javacourses/learning/PhoneBillCalculator.java
@@ -1,8 +1,41 @@
 package com.javacourses.learning;
 
+import java.util.Scanner;
+
 public class PhoneBillCalculator {
 
-    public static void main(String[] args) {
+    private static double overageFee = 0.25;
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        double baseCost = getBaseCost();
+        double overageMinutes = getOverageMinutes();
+        double tax = getTax(baseCost + overageMinutes * overageFee);
+        showPhoneBillStatement(baseCost, overageMinutes, tax);
+
+        scanner.close();
     }
+
+    public static void showPhoneBillStatement(double planCost, double overageMinutes, double tax) {
+        System.out.println("Phone Bill Statement");
+        System.out.println("Plan: $" + String.format("%.2f", planCost));
+        System.out.println("Overage: $" + String.format("%.2f", overageMinutes * overageFee));
+        System.out.println("Tax: " + String.format("%.2f", tax));
+        System.out.println("Total: " + String.format("%.2f", (planCost + overageMinutes * overageFee + tax)));
+    }
+
+    public static double getBaseCost() {
+        System.out.println("Enter base cost of the plan:");
+        return scanner.nextDouble();
+    }
+
+    public static double getOverageMinutes() {
+        System.out.println("Enter overage minutes:");
+        return scanner.nextDouble();
+    }
+
+    public static double getTax(double subtotal) {
+        return subtotal * 0.15;
+    }
+
 }
